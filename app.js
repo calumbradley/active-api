b473f7567bc6fe4bc937bcef6d605586fd4f7065
Calum Bradley <cal@gmx.co.uk>
2022-09-30 20:52:05 +0100

    bring in mysql and setup

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
-
+const mysql = require('mysql');
 
 const app = express();
 
@@ -9,6 +9,18 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+require('dotenv').config()
+
+const con = mysql.createConnection({
+  host: "active-api.ckgwmrigl9vq.us-east-1.rds.amazonaws.com",
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPW
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
 app.get('/exercise', (req, res) => {
   res.send('exercise')
